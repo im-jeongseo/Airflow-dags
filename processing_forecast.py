d140858698042f93a3cef9a23e1b59f2537b7158
im-jeongseo <ijs94@naver.com>
2024-06-07 14:10:20 +0900

    commit

--- processing_forecast.py
+++ processing_forecast.py
@@ -11,14 +11,14 @@ from pandas import json_normalize
 import pandas as pd
 from datetime import datetime
 
-#from sklearn.model_selection import train_test_split
-#from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 
-#import statsmodels.api as sm
-#from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-#from statsmodels.tsa.arima.model import ARIMA
-#from statsmodels.tsa.statespace.sarimax import SARIMAX
-#from pmdarima.arima import auto_arima
+import statsmodels.api as sm
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from pmdarima.arima import auto_arima
 
 from datetime import datetime, timedelta
 import itertools
@@ -30,14 +30,6 @@ import os
 
 import requests
 from bs4 import BeautifulSoup as bs
-
-def dag_function(**kwargs):
-    import sys
-    import subprocess
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'packagename'])
-
-    import packagename
-    # ... Use package
 
 def fetch_data_from_postgres(**context):
     # Initialize PostgresHook
@@ -78,6 +70,45 @@ def process_data_from_xcom(**context):
 
     df = df_init.set_index(keys='date')
     print(df)
+    
+    # train,test split
+    train_data, test_data = train_test_split(df, test_size=0.3, shuffle=False)
+    dt=datetime.datetime.today()
+    
+    # 예측을 위한 date index 생성
+    data_idx=[]
+    for i in range(10):
+        delta = datetime.timedelta(days = i)
+        dtnew = dt + delta
+        data_idx.append(str(dtnew))
+    
+    # ARIMA 모델 생성
+    p = range(0, 2)
+    d = range(1, 3)
+    q = range(0, 2)
+    pdq = list(itertools.product(p, d, q))
+
+    AIC = []
+    for i in pdq :
+        model = ARIMA(train_data['Close'].values, order=(i))
+        model_fit = model.fit()
+        print(f'ARIMA pdq : {i} >> AIC : {round(model_fit.aic, 2)}')
+        AIC.append(round(model_fit.aic, 2))
+
+    optim = [(pdq[i], j) for i, j in enumerate(AIC) if j == min(AIC)]
+
+    model = ARIMA(train_data['Close'].values, order=optim[0][0])
+    model_fit = model.fit()    
+
+    # 예측값 생성
+    pred = model_fit.get_forecast(len(test_data) +10)
+    pred_val = pred.predicted_mean
+
+    pred_index = list(test_data.index)
+    for i in date_idx:
+        pred_index.append(i)
+    
+    print(pred_val)
 
 dag = DAG(
     'processing_forecast',
