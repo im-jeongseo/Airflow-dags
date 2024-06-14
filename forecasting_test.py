a79b3314e65992caf2b5efad347f60d0f41f8f0d
im-jeongseo <ijs94@naver.com>
2024-06-14 10:50:41 +0900

    commit

--- forecasting_test.py
+++ forecasting_test.py
@@ -182,8 +182,8 @@ dag = DAG(
     default_args=default_args,
     description='stock data reprocessing & forecasting',
     tags=["postgresql", "reprocessing", "forecasting","PythonOperator","xcom"],
-    #schedule_interval='15 10 * * *',
-    schedule_interval='None',
+    schedule_interval='15 10 * * *',
+    #schedule_interval='None',
 )
 
 creating_table = PostgresOperator(
--- kafka_batch.py
+++ kafka_batch.py
@@ -18,8 +18,8 @@ dag = DAG(
     default_args=default_args,
     description='kafka data pull&push BashOperator',
     tags=["yahoo_finance", "kafka", "conumer","producer","BashOperator"],
-    #schedule_interval='0 10 * * *',
-    schedule_interval='None',
+    schedule_interval='0 10 * * *',
+    #schedule_interval='None',
 )
 
 # 각 Python 스크립트를 실행하는 BashOperator
